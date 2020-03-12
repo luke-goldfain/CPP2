8246c60b6ba1bb610c0297cd89074d8b1db244ca
luke-goldfain <lukegoldfain@gmail.com>
2020-03-11 21:22:52 -0500

    add gtest

---
+++ in-class/C++2_class6/sqrtpow/sqrtpow.hpp
@@ -0,0 +1,7 @@
+#ifndef SQRTPOW_H
+#define SQRTPOW_H
+
+double sqrt(double entry);
+double pow(double entry, int power);
+
+#endif
---
+++ in-class/C++2_class6/sqrtpow/test/tests.cpp
@@ -0,0 +1,15 @@
+//tests.cpp
+
+#include "../sqrtpow.hpp"
+#include <gtest/gtest.h>
+
+TEST(POWTEST, )
+{
+
+}
+
+int main(int argc, char ** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
