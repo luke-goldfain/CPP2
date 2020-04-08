08e792a341fb2d84f8ca7b4fcffcbfbea94447cb
Luke Goldfain <lukegoldfain@gmail.com>
2020-04-08 16:56:35 -0500

    Add tests

--- in-class/C++2_class6/sqrtpow/test/tests.cpp
+++ in-class/C++2_class6/sqrtpow/test/tests.cpp
@@ -1,11 +1,18 @@
 //tests.cpp
 
 #include "../sqrtpow.hpp"
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
-TEST(POWTEST, )
+TEST(POWTEST, Powers)
 {
+    EXPECT_EQ(25, pow(5, 2));
+    EXPECT_EQ(1728, pow(12, 3));
+}
 
+TEST(SQRTTEST, Sqrts)
+{
+    EXPECT_EQ(5, sqrt(25));
+    EXPECT_EQ(2.83, sqrt(8));
 }
 
 int main(int argc, char ** argv)
